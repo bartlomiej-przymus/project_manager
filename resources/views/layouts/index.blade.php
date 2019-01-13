499920dbce72172e7932a48a541a398fae7f60c0
Bartlomiej Przymus <bartlomiej.przymus@gmail.com>
2019-01-13 00:02:20 +0000

    created styling for authorization views created main page scafforlding

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+        <title>Project Manager</title>
+    </head>
+    <body>
+        @yield('content')
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    </body>
+</html>
